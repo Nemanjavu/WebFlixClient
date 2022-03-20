76ef54faa14d3cab0bfb337d55366dd87350a303
ZaraKinneen <zarakinneen@gmail.com>
2022-03-20 18:14:39 +0000

    Add project files.

---
+++ WebFlixClient/Models/MovieCatalogue.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace WebFlix.Models
+{
+    public class MovieCatalogue
+    {
+        [Key]
+        public int MovieId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        
+        public enum Genres { 
+            action ,
+            adventure , 
+            animation,
+            comedy ,
+            crime ,
+            drama ,
+            fantasy ,
+            family ,
+            horror ,
+            scifi ,
+            thriller 
+        }
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public Genres Genre { get; set; }
+        
+
+        public enum Certifications { 
+            universal, 
+            _12, 
+            _15, 
+            _18}
+
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public Certifications Certification { get; set;}
+        
+        public string ReleaseDate { get; set; }
+        
+        [Required]
+        [MinLength(1)]
+        [MaxLength(10)]
+        public int Rating { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("Id: {0} | Title: {1} | Genre: {2} | Certification: {3} | Release Date: {4} | Rating: {5}", MovieId, Title, Genre, Certification, ReleaseDate, Rating);
+        }
+    }
+}
---
+++ WebFlixClient/Program.cs
@@ -0,0 +1,44 @@
+﻿using WebFlix.Models;
+using static WebFlix.Models.MovieCatalogue;
+
+namespace WebFlixClient
+{
+    public class Program
+    {
+        static void Main()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:5041/");
+
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            GetAllMovies(client).Wait();
+            
+            MovieCatalogue entry = new MovieCatalogue() { MovieId = 5, Title = "Batman", Genre = Genres.action, Certification = Certifications._15, Rating = 9, ReleaseDate = "2022" };
+
+           
+
+        }
+
+        private static async Task GetAllMovies(HttpClient client)
+        {
+            HttpResponseMessage response = await client.GetAsync("api/Movie");
+            if (response.IsSuccessStatusCode)
+            {
+
+                var allMovies = await response.Content.ReadAsAsync<IEnumerable<MovieCatalogue>>();
+                foreach (var movieCatalogue in allMovies)
+                {
+                    Console.WriteLine(movieCatalogue);
+                }
+
+
+            }
+        }
+
+
+
+
+        
+    }
+        
+    }
